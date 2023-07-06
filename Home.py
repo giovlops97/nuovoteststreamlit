135ac2055d318ad37da851b64f91333c4684c348
giovlops97 <giovlops97@gmail.com>
2023-07-06 12:55:39 +0200

    init repo

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Hello this is Errore Bizantino")
 st.image("https://media.tenor.com/x-kqDAmw2NQAAAAC/parrot-party.gif")
-selected_option = st.selectbox("Scegli l'animale:", ["Pappagallo", "Maiale"]
+selected_option = st.selectbox("Scegli la madonna:", ["Pappagallo", "Maiale"]
 )
